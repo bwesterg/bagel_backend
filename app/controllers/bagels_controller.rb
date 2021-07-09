a27417c019f56404c7a3000f18ede67b51ecd744
bwesterg <benton.westergaard@gmail.com>
2021-07-09 16:33:37 -0600

    Connected backend url to front.

---
+++ app/controllers/bagels_controller.rb
@@ -0,0 +1,9 @@
+class BagelsController < ApplicationController
+
+
+    def index
+        @bagels = Bagel.all 
+        render json: @bagels
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Bagel.destroy_all
+
+
+Bagel.create([
+    {
+        name: "Plain",
+        rating: 4,
+    },
+    {
+        name: "Everything",
+        rating: 3,
+    },
+    {
+        name: "4 Cheese",
+        rating: 8,
+    },
+    {
+        name: "Gluten-Free",
+        rating: 1,
+    },
+])
