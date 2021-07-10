d3bf70686d21aa5740c61b542471056bd2fe6fb7
bwesterg <benton.westergaard@gmail.com>
2021-07-10 14:29:05 -0600

    Added create method for new bagels.

--- app/controllers/bagels_controller.rb
+++ app/controllers/bagels_controller.rb
@@ -6,4 +6,15 @@ class BagelsController < ApplicationController
         render json: @bagels
     end
 
+    def create
+        @bagel = Bagel.create(bagel_params)
+        render json: @bagel
+    end
+
+    private
+
+    def bagel_params
+        params.require(:bagel).permit(:name, :rating)
+    end
+
 end
